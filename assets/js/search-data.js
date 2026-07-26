ac2eb744be6c024fa6e695251743688f9cf4718a
afan104 <101306676+afan104@users.noreply.github.com>
2026-07-26 05:38:29 +0000

    Deploying to gh-pages from @ afan104/afan104.github.io@58ba02be47025f46f9013c72e507cebc042c0f33 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A selection of side projects spanning backend systems, cloud infrastructure, and computer vision.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
